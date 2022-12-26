45bd3c61badc2b15e1bf39ed7545c5cdf8b83515
Pluto-Fsr <119164227+Pluto-Fsr@users.noreply.github.com>
2022-12-26 21:57:37 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -36,14 +36,14 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-04-26' },
+        { keyword: 'love_day', date: '2022-03-15' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: '0001',
 
   CALLBACK_USERS: [
     {
