bc343d3e1da53527aec1b3b91401ffb3f41094ba
Pluto-Fsr <119164227+Pluto-Fsr@users.noreply.github.com>
2022-12-26 21:52:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oeVv56a2bOTJrlcDOYwleGRBKqN8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '7Ck1A31zLMeZd_p1SD8oIbELRT-RkwVaMqx4aZWQA_Y',
+      useTemplateId: 'AphL0NrKFigBGYarNyDsIyP-wM--9-2SKaj7id3D9i0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
       festivals: [
