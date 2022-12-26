c12131fee7b1e8b67e11c6e4f9b7b55b91c75075
Pluto-Fsr <119164227+Pluto-Fsr@users.noreply.github.com>
2022-12-26 21:11:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,6 +15,15 @@ const USER_CONFIG = {
 
   PROVINCE: '山东',
   CITY: '青岛',
+  
+    TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '2a4aa4c3821962acb70fe4751e3809ee',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+    },
 
   USERS: [
     {
@@ -23,7 +32,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oeVv56einF6XvOJPhAUkm-oAMP1I',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '6A_JDKujXCy9rlDhmf7K_kzyskz6euc_SnuWpzBAiIQ',
+      useTemplateId: 'PoHnmmdW5wrVCML-MR2W91r7BIxuDPpLSw_bfWr3WX0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-29',
       festivals: [
