7f701592415d962606f81d9572843cd418964696
Pluto-Fsr <119164227+Pluto-Fsr@users.noreply.github.com>
2022-12-26 20:37:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
   
   TIAN_API: {
     // 天行API KEY，如果使用天行API则需要填写此项
-    key: '',
+    key: '2a4aa4c3821962acb70fe4751e3809ee',
     
     /** 天行API相关，需要config中配置 TIAN_API_KEY  */
     // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
