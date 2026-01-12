756b921d501f550853b09f0d8f0dbd99d3c62214
Pluto-Fsr <119164227+Pluto-Fsr@users.noreply.github.com>
2026-01-12 19:14:38 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,11 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '洗衣粉儿',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oeVv56a2bOTJrlcDOYwleGRBKqN8',
+      id: 'ok0W02yQlvuTxWsVduIXOP1gov20',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ib1Yig0REX_gN4I346lwxocc8qXLTx9VSWatuUcm558',
+      useTemplateId: 'aeCw483OTZLZuFygiQvspPs0HTAQsJxScf99nH8-MYs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '03-29',
+      horoscopeDate: '02-09',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
